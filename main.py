b10838ec25a4dc3b0e280b2274f56e70d87f205f
coderx <36203292+blcx@users.noreply.github.com>
2021-06-12 07:29:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+response = requests.get('https://api.stackexchange.com//2.2/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    print(data['link'])
